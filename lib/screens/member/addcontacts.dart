3933ef2b44823265a08d4f364287abf303af0572
dreamhigh9990 <dreamhigh9990@gmail.com>
2024-02-20 09:26:29 +0900

    add contact & member page

---
+++ lib/screens/member/addcontacts.dart
@@ -0,0 +1,241 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced_avatar/flutter_advanced_avatar.dart';
+
+import 'package:pardna/screens/home.dart';
+import 'package:pardna/utils/text_utils.dart';
+import 'package:pardna/utils/headers.dart';
+
+import 'package:stylish_bottom_bar/model/bar_items.dart';
+import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
+
+class AddContacts extends StatefulWidget {
+  const AddContacts({Key? key}) : super(key: key);
+
+  @override
+  State<AddContacts> createState() => _AddContactsState();
+}
+
+class _AddContactsState extends State<AddContacts> {
+  final ExpansionTileController controller = ExpansionTileController();
+  List<Contact> contacts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData(); // Perform side effect on widget initialization
+  }
+
+  fetchData() async {
+    if (await FlutterContacts.requestPermission()) {
+      List<Contact> data = await FlutterContacts.getContacts(
+          withProperties: true, withPhoto: true);
+      setState(() {
+        contacts = data;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/homebg.jpg"), fit: BoxFit.fill),
+          ),
+          child: Column(
+            children: [
+              DetailHeader(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 43,
+                      padding: const EdgeInsets.only(left: 10),
+                      decoration: const BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(color: Colors.black38))),
+                      child: TextFormField(
+                        style: const TextStyle(color: Colors.black),
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(
+                            Icons.close,
+                            color: Colors.black38,
+                          ),
+                          hintStyle: TextStyle(fontSize: 16),
+                          hintText: 'Search users by name',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    SizedBox(
+                      height: 600,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            for (int i = 0; i < contacts.length; i++)
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8),
+                                child: Row(
+                                  children: [
+                                    const AdvancedAvatar(
+                                      size: 50,
+                                      decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: Colors.green),
+                                      child: Icon(
+                                        Icons.account_circle,
+                                        color: Colors.white,
+                                        size: 30,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 5),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        TextUtil(
+                                          text: contacts[i].displayName,
+                                          size: 13,
+                                          weight: true,
+                                          color: Colors.black,
+                                        ),
+                                        TextUtil(
+                                          text: contacts[i].emails.isNotEmpty
+                                              ? contacts[i].emails[0].address
+                                              : 'No email address',
+                                          size: 12,
+                                          color: Colors.black87,
+                                        ),
+                                        TextUtil(
+                                          text: contacts[i].phones.isNotEmpty
+                                              ? contacts[i].phones[0].number
+                                              : 'No phone number',
+                                          size: 12,
+                                          color: Colors.black87,
+                                        ),
+                                      ],
+                                    ),
+                                    const Spacer(),
+                                    TextButton(
+                                      child: Container(
+                                        width: 40,
+                                        height: 40,
+                                        decoration: BoxDecoration(
+                                            color: Colors.green,
+                                            borderRadius:
+                                                BorderRadius.circular(20)),
+                                        child: const Icon(
+                                          Icons.add,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                      onPressed: () {},
+                                    ),
+                                  ],
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: StylishBottomBar(
+        option: AnimatedBarOptions(
+          iconSize: 40,
+        ),
+        backgroundColor: Colors.green,
+        items: [
+          BottomBarItem(
+            icon: const Icon(Icons.home),
+            backgroundColor: Colors.black,
+            title: const TextUtil(
+              text: 'Home',
+              size: 15,
+              color: Colors.black,
+              weight: true,
+            ),
+          ),
+          BottomBarItem(
+            icon: const Icon(Icons.groups),
+            backgroundColor: Colors.black,
+            title: const TextUtil(
+              text: 'Member',
+              size: 15,
+              color: Colors.black,
+              weight: true,
+            ),
+          ),
+          BottomBarItem(
+            icon: const Icon(Icons.task),
+            backgroundColor: Colors.black,
+            title: const TextUtil(
+              text: 'Projects',
+              size: 15,
+              color: Colors.black,
+              weight: true,
+            ),
+          ),
+        ],
+        hasNotch: true,
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomePage(pageIndex: 0),
+                ),
+              );
+              break;
+            case 1:
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomePage(pageIndex: 2),
+                ),
+              );
+              break;
+            case 2:
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomePage(pageIndex: 1),
+                ),
+              );
+              break;
+            default:
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        enableFeedback: false,
+        onPressed: () {},
+        backgroundColor: Colors.green,
+        child: const Icon(
+          Icons.groups,
+          size: 40,
+          color: Colors.black,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
